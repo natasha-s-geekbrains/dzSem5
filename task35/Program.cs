8183bbf0ede89c8844d8b8b9cc28f3868b353095
natasha_s <natasl1012@gmail.com>
2023-07-22 00:21:12 +0300

    added tasks 35,37

---
+++ task35/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в 
+// отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void ShowResult(int[]array, int newMin, int newMax)
+{
+    int count = 0;
+    foreach(int el in array)
+    {
+        if (el >= newMin && el <= newMax)
+        {
+            count++;
+        }
+    }
+    Console.Write($"Количество элементов массива, значения которых лежат в отрезке [10,99] = {count}");
+}
+
+int arrSize = 123;
+int minValue = GetNum("Введите значение нижней границы массива: ");
+int maxValue = GetNum("Введите значение верхней границы массива: ");
+
+//Console.Write($"{arrSize}, {minValue}, {maxValue}");
+int[] arr = GetArray(arrSize, minValue, maxValue);
+PrintArray(arr);
+
+int newMin = 10;
+int newMax = 99;
+
+ShowResult(arr, newMin, newMax);
---
+++ task37/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний 
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] FillArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] ModifyArray(int[] array)
+{
+    int newArrSize = 0;
+    if (array.Length % 2 == 0)
+    {
+        newArrSize = array.Length / 2;
+    }
+    else
+    {
+        newArrSize = array.Length / 2 + 1;
+    }
+    int[] resultArr = new int[newArrSize];
+
+    if (array.Length % 2 == 0)
+    {
+        for (int j = 0; j < newArrSize; j++)
+        {
+            resultArr[j] = array[j] * array[array.Length - 1 - j];
+        }
+    }
+    else
+    {
+        for (int j = 0; j < newArrSize; j++)
+        {
+            resultArr[j] = array[j] * array[array.Length - 1 - j];
+            resultArr[newArrSize - 1] = array[array.Length / 2];
+        }
+    }
+    return resultArr;
+}
+
+int arrSize = GetNum("Введите длину массива: ");
+int arrMinValue = GetNum("Введите значение нижней границы массива: ");
+int arrMaxValue = GetNum("Введите значение верхней границы массива: ");
+
+int[] arr = FillArray(arrSize, arrMinValue, arrMaxValue);
+
+PrintArray(arr);
+
+int[] resultArray = ModifyArray(arr);
+PrintArray(resultArray);
+
